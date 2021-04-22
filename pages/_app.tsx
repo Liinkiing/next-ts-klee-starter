739157e87a6a0b95bfff36fb26f4de965371dc7e
Omar Jbara <omar.jbara2@gmail.com>
2021-04-22 15:29:31 +0200

    - Added global styles for emotion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import NProgress from '~/components/NProgress'
 import AppNav from '~/components/layout/AppNav'
 import { AnimatePresence } from 'framer-motion'
 import { KleeProvider, useTheme } from '@liinkiing/klee'
+import GlobalStyles from '~/styles/Global'
 
 const MyApp: FC<AppProps> = ({ Component, pageProps, router }) => {
   const theme = useTheme()
 
   return (
     <>
+      <GlobalStyles />
       <NProgress color={theme.colors.red['400']} spinner={false} />
       <AppNav />
       <AnimatePresence exitBeforeEnter initial={false}>
---
+++ src/styles/Global.tsx
@@ -0,0 +1,8 @@
+import { css, Global } from '@emotion/react'
+import * as React from 'react'
+
+const styles = css``
+
+export const GlobalStyles = () => <Global styles={styles} />
+
+export default GlobalStyles
